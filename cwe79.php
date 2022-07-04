75db21da22048ec272fea99e43e1fdd99b751618
Tim Benning <tim.benning@protonmail.com>
2022-07-04 15:17:29 +0200

    Tested FILTER_SANITIZE_STRING

--- cwe79.php
+++ cwe79.php
@@ -1,6 +1,13 @@
 <?php
-// CWE 79
     $username = $_GET['username'];
-    $encodedUsername = filter_var($username, FILTER_FLAG_ENCODE_HIGH)
-    echo '<div class="header"> Welcome, ' . $encodedUsername . '</div>';
+    $stripLowUsername = filter_var($username, FILTER_FLAG_STRIP_LOW)
+    echo '<div class="header"> Welcome, ' . $stripLowUsername . '</div>';
+    $stripHighUsername = filter_var($username, FILTER_FLAG_STRIP_HIGH)
+    echo '<div class="header"> Welcome, ' . $stripHighUsername . '</div>';
+    $encodedLowUsername = filter_var($username, FILTER_FLAG_ENCODE_LOW)
+    echo '<div class="header"> Welcome, ' . $encodedLowUsername . '</div>';
+    $encodedHighUsername = filter_var($username, FILTER_FLAG_ENCODE_HIGH)
+    echo '<div class="header"> Welcome, ' . $encodedHighUsername . '</div>';
+    $encodedAmpUsername = filter_var($username, FILTER_FLAG_ENCODE_AMP);
+    echo '<div class="header"> Welcome, ' . $encodedAmpUsername . '</div>';
 ?>
