b88b98015880e2ba4f662ce2af64a7f56eefccd1
Tim Benning <tim.benning@protonmail.com>
2022-07-04 18:46:52 +0200

    removed comment

--- cwe79test.php
+++ cwe79test.php
@@ -4,7 +4,6 @@
     }
 
     $username = $_GET['username'];
-    //echo '<div class="header"> Welcome, ' . $username . '</div>';
     $sanitizedUsername = useless_filter($username);
     echo '<div class="header"> Welcome, ' . $sanitizedUsername . '</div>';
 ?>
