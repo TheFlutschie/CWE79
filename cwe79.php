27b80f72f11d7a0aca355c508a5f4f092009ff5f
Tim Benning <tim.benning@protonmail.com>
2022-07-04 15:47:21 +0200

    Fixed FILTER_SANITIZE_STRING

--- cwe79.php
+++ cwe79.php
@@ -1,13 +1,16 @@
 <?php
     $username = $_GET['username'];
-    $stripLowUsername = filter_var($username, FILTER_FLAG_STRIP_LOW)
+    echo '<div class="header"> Welcome, ' . $username . '</div>';
+    $noEncodeUsername=filter_var($username,FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+    echo '<div class="header"> Welcome, ' . $noEncodeUsername . '</div>';
+    $stripLowUsername = filter_var($username,FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
     echo '<div class="header"> Welcome, ' . $stripLowUsername . '</div>';
-    $stripHighUsername = filter_var($username, FILTER_FLAG_STRIP_HIGH)
+    $stripHighUsername = filter_var($username,FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
     echo '<div class="header"> Welcome, ' . $stripHighUsername . '</div>';
-    $encodedLowUsername = filter_var($username, FILTER_FLAG_ENCODE_LOW)
+    $encodedLowUsername = filter_var($username,FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_LOW);
     echo '<div class="header"> Welcome, ' . $encodedLowUsername . '</div>';
-    $encodedHighUsername = filter_var($username, FILTER_FLAG_ENCODE_HIGH)
+    $encodedHighUsername = filter_var($username,FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_HIGH);
     echo '<div class="header"> Welcome, ' . $encodedHighUsername . '</div>';
-    $encodedAmpUsername = filter_var($username, FILTER_FLAG_ENCODE_AMP);
+    $encodedAmpUsername = filter_var($username,FILTER_SANITIZE_STRING, FILTER_FLAG_ENCODE_AMP);
     echo '<div class="header"> Welcome, ' . $encodedAmpUsername . '</div>';
 ?>
